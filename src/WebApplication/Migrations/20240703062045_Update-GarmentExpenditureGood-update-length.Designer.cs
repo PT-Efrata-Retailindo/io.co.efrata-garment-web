6d12076c4149d3720529d98707a10a796de2284e
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-07-03 14:22:20 +0700

    2024/14/Enhance/Enhance-Max-Length-Article-ExpenditureGood

--- src/WebApplication/Migrations/20240703062045_Update-GarmentExpenditureGood-update-length.Designer.cs
+++ src/WebApplication/Migrations/20240703062045_Update-GarmentExpenditureGood-update-length.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20240703062045_Update-GarmentExpenditureGood-update-length")]
+    partial class UpdateGarmentExpenditureGoodupdatelength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
