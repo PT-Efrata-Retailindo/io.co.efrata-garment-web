0a627b87d565f7ca4cb173847ad43551fce9b1eb
Refido Bagas Prakosa <refidobagas2607@gmail.com>
2025-06-26 10:33:59 +0700

    Migration Database Racking Inventory ( EMS ) - Garment

---
+++ src/WebApplication/Migrations/20250626031951_AddColoumnForRackingInventory.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DanLiris.Admin.Web.Migrations
+{
+    public partial class AddColoumnForRackingInventory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Area",
+                table: "GarmentSampleDeliveryReturnItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Box",
+                table: "GarmentSampleDeliveryReturnItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Colour",
+                table: "GarmentSampleDeliveryReturnItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Level",
+                table: "GarmentSampleDeliveryReturnItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rack",
+                table: "GarmentSampleDeliveryReturnItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Area",
+                table: "GarmentDeliveryReturnItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Box",
+                table: "GarmentDeliveryReturnItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Colour",
+                table: "GarmentDeliveryReturnItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Level",
+                table: "GarmentDeliveryReturnItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rack",
+                table: "GarmentDeliveryReturnItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "GarmentSampleDeliveryReturnItems");
+
+            migrationBuilder.DropColumn(
+                name: "Box",
+                table: "GarmentSampleDeliveryReturnItems");
+
+            migrationBuilder.DropColumn(
+                name: "Colour",
+                table: "GarmentSampleDeliveryReturnItems");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "GarmentSampleDeliveryReturnItems");
+
+            migrationBuilder.DropColumn(
+                name: "Rack",
+                table: "GarmentSampleDeliveryReturnItems");
+
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "GarmentDeliveryReturnItems");
+
+            migrationBuilder.DropColumn(
+                name: "Box",
+                table: "GarmentDeliveryReturnItems");
+
+            migrationBuilder.DropColumn(
+                name: "Colour",
+                table: "GarmentDeliveryReturnItems");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "GarmentDeliveryReturnItems");
+
+            migrationBuilder.DropColumn(
+                name: "Rack",
+                table: "GarmentDeliveryReturnItems");
+        }
+    }
+}
